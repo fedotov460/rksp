7f338e8b11dad4c169718291229858b71fb49693
Fedotov <fedotov460@example.com>
2025-12-16 13:58:36 +0400

    Initial commit: Generated Spring Boot project with dependencies

---
+++ src/main/java/ru/rksp/Fedotov/FedotovApplication.java
@@ -0,0 +1,13 @@
+package ru.rksp.Fedotov;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FedotovApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FedotovApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/rksp/Fedotov/FedotovApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.rksp.Fedotov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FedotovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
